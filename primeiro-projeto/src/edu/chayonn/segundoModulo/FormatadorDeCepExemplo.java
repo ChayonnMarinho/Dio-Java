37045bd3a4d97b2814e4a1be823d0073f5e4c5fd
ChayonnMarinho <chayonnmar@gmail.com>
2023-07-27 11:55:05 -0300

    tratamento de exceção

---
+++ primeiro-projeto/src/edu/chayonn/segundoModulo/FormatadorDeCepExemplo.java
@@ -0,0 +1,21 @@
+package edu.chayonn.segundoModulo;
+
+public class FormatadorDeCepExemplo {
+
+    public static void main(String[] args) {
+
+        try {
+            String cepFormatado = formatarCep("23765064");
+            System.out.println(cepFormatado);
+        } catch (CepInvalidoException e) {
+            System.out.println("O Cep não corresponde com as regras de negócio.");;
+        }
+        
+    }
+
+    static String formatarCep(String cep) throws CepInvalidoException{
+        if(cep.length()!=8)
+            throw new CepInvalidoException();
+            return "23.765-064";
+        }    
+}
